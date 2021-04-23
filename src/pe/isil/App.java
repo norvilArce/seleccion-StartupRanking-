a245460751febbbf160b6758387dc77d1f486375
Norvil Arce <norvil.arce@gmail.com>
2021-04-22 21:00:41 -0500

    agrego switch con opcion para name y xpath

--- src/pe/isil/App.java
+++ src/pe/isil/App.java
@@ -1,36 +1,37 @@
 package pe.isil;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App {
+    static WebDriver driver;
     public static void main(String[] args) {
+        String baseURL = "https://apprater.net/add/";
         //ruta del archivo chromedriver
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\drivers\\chromedriver.exe");
-
-        sendText("Your Name", "xpath", "user-submitted-name", "Lucas Ocampo");
-    }
-
-    public static void sendText(String label, String selectorType, String selector, String value) {
-        WebDriver driver;
-        String baseURL = "https://apprater.net/add/";
 
         driver = new ChromeDriver();    //abrir navegador
         driver.manage().window().maximize();//maximizar
         driver.get(baseURL);            //navegar a la pagina
 
-        WebElement element = null;
+        //lleno campos de formulario
+        sendText("Your Name", "name", "user-submitted-name", "Lucas Ocampo");
+        sendText("Email Address", "xpath", "//*[@id=\"usp_form\"]/div[3]/div[2]/fieldset/input", "l.ocampo@lucasmail.com");
+    }
+
+    public static void sendText(String label, String selectorType, String selector, String value) {
+        WebElement element=null;
 
         switch (selectorType) {
             case "css":
-
                 break;
             case "name":
-
+                element = driver.findElement(By.name(selector));
                 break;
             case "xpath":
-
+                element = driver.findElement(By.xpath(selector));
                 break;
         }
 
